5f0e41a51402882c0f4c1a1c6e392ecc32851c4c
Marcio Santiago <marcio.santiago@kaufda.de>
2015-04-02 12:24:59 -0300

    Improvements suggested by @PauloSuzart

---
+++ src/guess/client/dto/Guess.java
@@ -0,0 +1,38 @@
+package guess.client.dto;
+
+import java.io.Serializable;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author marcio
+ */
+public class Guess implements Serializable {
+    
+    private int id;
+    private int guessNumber;
+    
+    private static final long serialVersionUID = 5627856554022993232L;
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getGuessNumber() {
+        return guessNumber;
+    }
+
+    public void setGuessNumber(int guessNumber) {
+        this.guessNumber = guessNumber;
+    }  
+    
+}
